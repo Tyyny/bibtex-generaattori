dfbefea395f78ea020947e9492a7d29b7c348ea2
Simo Soini <74994300+Capslock01@users.noreply.github.com>
2022-12-14 00:03:26 +0200

    Added find to debug robot test failing in CI

--- run_robot_tests.sh
+++ run_robot_tests.sh
@@ -10,6 +10,8 @@ poetry run robot src/tests/robottest.robot
 poetry run robot src/tests/user_can_add_reference_type_book.robot
 status=$?
 
+find / -type f -print 2>/dev/null | grep references.bib
+
 kill $(lsof -t -i:5000)
 
 exit $status
