5e191ff637bcf3f57c3ea58f6801b2a2f73fa2a0
Olavi Pakola <paahdin.com@hotmail.com>
2022-12-13 17:35:17 +0200

    added functionality for editing references

--- src/routes.py
+++ src/routes.py
@@ -34,8 +34,6 @@ def send_reference(ref_type: str):
     """New reference page."""
     if request.method == 'GET':
         return render_template('send_reference.html', ref_type=ref_type)
-    if request.method == 'POST':
-        return redirect(f'/edit/{ref_type}')
     else:
         ref_type = request.form['ref_type']
         author = request.form['author']
@@ -53,6 +51,41 @@ def send_reference(ref_type: str):
                 booktitle,
                 pages
             )
+    return redirect('/')
+
+@app.route('/edit_reference/<id>', methods=['GET', 'POST'])
+def edit_reference(id: int):
+    """Pre-filled form for editing a reference"""
+    id = request.form['id']
+    ref = service.get_reference_by_id(id)
+    return render_template('edit_reference.html', ref_id=id,
+                                                    ref_type=ref.type.name,
+                                                    author=ref.author,
+                                                    title=ref.title,
+                                                    year=ref.year,
+                                                    booktitle=ref.booktitle,
+                                                    pages=ref.pages)
+    
+@app.route('/edited', methods=['GET', 'POST'])
+def edited_ref_to_database():
+    ref_id = request.form['id']
+    ref_type = request.form['ref_type']
+    author = request.form['author']
+    title = request.form['title']
+    year = request.form['year']
+    if ref_type == "inCollection":
+        service.edit_reference(ref_id, author, title, year)
+    if ref_type == "book":
+        booktitle = request.form['booktitle']
+        pages = request.form['pages']
+        service.edit_reference_book(
+            ref_id,
+            author,
+            title,
+            year,
+            booktitle,
+            pages
+        )
     return redirect('/')
 
 @app.route('/download', methods=['POST'])
